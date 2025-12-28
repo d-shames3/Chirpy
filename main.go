61b6372496271da81d668213d8d147fc46512696
David Shames <dshames3@gmail.com>
2025-12-28 14:39:15 -0500

    feat: health endpoint

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	const healthOKResponse = "OK"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	healthHandlerFunc := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(healthOKResponse))
+	}
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", healthHandlerFunc)
 
 	server := &http.Server{
 		Addr:    ":" + port,
