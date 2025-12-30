b6868b895d85545e890b49a93ed56333ac4dca68
David Shames <dshames3@gmail.com>
2025-12-30 13:20:27 -0500

    feat: metrics endpoint

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
@@ -10,6 +12,7 @@ func main() {
 	const port = "8080"
 	const healthOKResponse = "OK"
 
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
 
 	healthHandlerFunc := func(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +21,12 @@ func main() {
 		w.Write([]byte(healthOKResponse))
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app/", cfg.middlewareMetricsInc(fileServerHandler))
 	mux.HandleFunc("/healthz", healthHandlerFunc)
+	mux.HandleFunc("/metrics", cfg.fileServerMetricsHandler)
+	mux.HandleFunc("/reset", cfg.resetMetricsHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -28,4 +35,28 @@ func main() {
 
 	log.Fatal(server.ListenAndServe())
 
+}
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) fileServerMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileServerHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Store(0)
 }
