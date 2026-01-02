7f69d311b9196f0c5bf0d0cf57f46caadd304e32
d-shames3 <dshames3@gmail.com>
2026-01-02 13:03:32 -0500

    chore: routing

--- main.go
+++ main.go
@@ -10,23 +10,19 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
-	const healthOKResponse = "OK"
 
-	cfg := &apiConfig{}
-	mux := http.NewServeMux()
-
-	healthHandlerFunc := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(healthOKResponse))
+	cfg := apiConfig{
+		fileServerHits: atomic.Int32{},
 	}
+
+	mux := http.NewServeMux()
 
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 
 	mux.Handle("/app/", cfg.middlewareMetricsInc(fileServerHandler))
-	mux.HandleFunc("/healthz", healthHandlerFunc)
-	mux.HandleFunc("/metrics", cfg.fileServerMetricsHandler)
-	mux.HandleFunc("/reset", cfg.resetMetricsHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandlerFunc)
+	mux.HandleFunc("GET /admin/metrics", cfg.fileServerMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetMetricsHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -49,14 +45,29 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) fileServerMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	hits := cfg.fileServerHits.Load()
-	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been viewed %d times!</p>
+		</body>
+	</html>
+	`,
+		hits)))
 }
 
 func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileServerHits.Store(0)
+	w.Write([]byte("Reset hits to 0"))
+}
+
+func healthHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
